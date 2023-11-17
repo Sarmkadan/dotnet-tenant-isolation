c4a515ed9b5a096ce2c1f1b4b46c9722176074e4
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-17 06:02:44 +0200

    feat(models): TenantConnectionString extensions

---
+++ src/TenantIsolation/Models/TenantConnectionStringExtensions.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace TenantIsolation.Models
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="TenantConnectionString"/> class.
+    /// </summary>
+    public static class TenantConnectionStringExtensions
+    {
+        /// <summary>
+        /// Validates the configuration of a <see cref="TenantConnectionString"/>.
+        /// </summary>
+        /// <param name="connectionString">The connection string to validate.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="connectionString"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if any required or invalid configuration is detected.</exception>
+        public static void Validate(this TenantConnectionString connectionString)
+        {
+            ArgumentNullException.ThrowIfNull(connectionString);
+
+            if (string.IsNullOrEmpty(connectionString.ServerHost))
+                throw new ArgumentException("ServerHost must not be null or empty.", nameof(connectionString.ServerHost));
+
+            if (connectionString.ServerPort is null or <= 0 or > 65535)
+                throw new ArgumentException("ServerPort must be between 1 and 65535.", nameof(connectionString.ServerPort));
+
+            if (string.IsNullOrEmpty(connectionString.DatabaseType))
+                throw new ArgumentException("DatabaseType must not be null or empty.", nameof(connectionString.DatabaseType));
+
+            if (connectionString.DatabaseType is not ("SqlServer" or "PostgreSQL" or "MySQL"))
+                throw new ArgumentException("DatabaseType must be a supported type: SqlServer, PostgreSQL, MySQL.", nameof(connectionString.DatabaseType));
+
+            if (!string.IsNullOrEmpty(connectionString.SchemaName) && connectionString.SchemaName.Length > 128)
+                throw new ArgumentException("SchemaName must not exceed 128 characters.", nameof(connectionString.SchemaName));
+        }
+
+        /// <summary>
+        /// Generates a human-readable display name for the <see cref="TenantConnectionString"/>.
+        /// </summary>
+        /// <param name="connectionString">The connection string to generate a display name for.</param>
+        /// <returns>A formatted string representing the connection string.</returns>
+        public static string ToDisplayName(this TenantConnectionString connectionString)
+        {
+            ArgumentNullException.ThrowIfNull(connectionString);
+
+            return string.IsNullOrEmpty(connectionString.Name)
+                ? $"{connectionString.ServerHost}:{connectionString.ServerPort} ({connectionString.DatabaseName})"
+                : $"{connectionString.Name} ({connectionString.ServerHost}:{connectionString.ServerPort})";
+        }
+    }
+}
