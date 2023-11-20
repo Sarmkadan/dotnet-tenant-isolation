c6925a7b5b428e0924577866159522b72aefecc4
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-20 12:09:57 +0200

    small TenantEvent helpers

--- src/TenantIsolation/Events/TenantEventExtensions.cs
+++ src/TenantIsolation/Events/TenantEventExtensions.cs
@@ -1,27 +1,47 @@
-using System;
+#nullable enable
 
 namespace TenantIsolation.Events
 {
+    /// <summary>
+    /// Extension methods for <see cref="TenantEvent"/> providing utility operations for event handling and type checking
+    /// </summary>
     public static class TenantEventExtensions
     {
+        /// <summary>
+        /// Gets a human-readable description of the tenant event including its type, ID, timestamp, and tenant information
+        /// </summary>
+        /// <param name="event">The tenant event to describe</param>
+        /// <returns>A formatted string containing event details</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="event"/> is <see langword="null"/></exception>
         public static string GetEventDescription(this TenantEvent @event)
         {
-            return $"EventId: {@event.EventId}, OccurredAt: {@event.OccurredAt}, TenantId: {@event.TenantId}";
+            ArgumentNullException.ThrowIfNull(@event);
+
+            return $"Event [{@event.GetType().Name}] - ID: {@event.EventId}, OccurredAt: {@event.OccurredAt:O}, TenantId: {@event.TenantId}";
         }
 
-        public static bool IsTenantActivatedEvent(this TenantEvent @event)
-        {
-            return @event is TenantActivatedEvent;
-        }
+        /// <summary>
+        /// Determines whether the event is a <see cref="TenantActivatedEvent"/> instance
+        /// </summary>
+        /// <param name="event">The tenant event to check</param>
+        /// <returns><see langword="true"/> if the event is a tenant activation event; otherwise, <see langword="false"/></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="event"/> is <see langword="null"/></exception>
+        public static bool IsTenantActivatedEvent(this TenantEvent @event) => @event is TenantActivatedEvent;
 
-        public static bool IsTenantSuspendedEvent(this TenantEvent @event)
-        {
-            return @event is TenantSuspendedEvent;
-        }
+        /// <summary>
+        /// Determines whether the event is a <see cref="TenantSuspendedEvent"/> instance
+        /// </summary>
+        /// <param name="event">The tenant event to check</param>
+        /// <returns><see langword="true"/> if the event is a tenant suspension event; otherwise, <see langword="false"/></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="event"/> is <see langword="null"/></exception>
+        public static bool IsTenantSuspendedEvent(this TenantEvent @event) => @event is TenantSuspendedEvent;
 
-        public static bool IsTenantDeletedEvent(this TenantEvent @event)
-        {
-            return @event is TenantDeletedEvent;
-        }
+        /// <summary>
+        /// Determines whether the event is a <see cref="TenantDeletedEvent"/> instance
+        /// </summary>
+        /// <param name="event">The tenant event to check</param>
+        /// <returns><see langword="true"/> if the event is a tenant deletion event; otherwise, <see langword="false"/></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="event"/> is <see langword="null"/></exception>
+        public static bool IsTenantDeletedEvent(this TenantEvent @event) => @event is TenantDeletedEvent;
     }
 }
