1e36dc7d8089cad24d2913cf0ae72bcab6ab6ed2
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-24 09:26:44 +0200

    a couple of Collection extensions

--- src/TenantIsolation/Utilities/CollectionExtensions.cs
+++ src/TenantIsolation/Utilities/CollectionExtensions.cs
@@ -16,6 +16,7 @@ public static class CollectionExtensions
     /// <summary>
     /// Check if collection is null or empty
     /// </summary>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/></exception>
     public static bool IsNullOrEmpty<T>(this IEnumerable<T>? collection)
     {
         return collection == null || !collection.Any();
@@ -24,38 +25,59 @@ public static class CollectionExtensions
     /// <summary>
     /// Check if collection has items
     /// </summary>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/></exception>
     public static bool HasItems<T>(this IEnumerable<T>? collection)
     {
         return collection != null && collection.Any();
     }
 
     /// <summary>
-    /// Get safe item at index, returns default if out of bounds
+    /// Get item at index safely, returns default if out of bounds
     /// Prevents IndexOutOfRangeException when accessing items
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <param name="collection">The collection to access</param>
+    /// <param name="index">The zero-based index of the item to retrieve</param>
+    /// <returns>The item at the specified index, or <see langword="null"/> if out of bounds or collection is null</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/></exception>
     public static T? SafeGetAt<T>(this IEnumerable<T> collection, int index) where T : class
     {
-        if (collection == null || index < 0)
+        ArgumentNullException.ThrowIfNull(collection);
+
+        if (index < 0)
             return null;
 
-        return collection.Skip(index).FirstOrDefault();
+        // Use ElementAtOrDefault which is more efficient than Skip(index).FirstOrDefault()
+        return collection.ElementAtOrDefault(index);
     }
 
     /// <summary>
     /// Add item to list if not already present
     /// </summary>
+    /// <typeparam name="T">The type of elements in the list</typeparam>
+    /// <param name="list">The list to modify</param>
+    /// <param name="item">The item to add</param>
+    /// <exception cref="ArgumentNullException"><paramref name="list"/> is <see langword="null"/></exception>
     public static void AddIfNotExists<T>(this IList<T> list, T item)
     {
-        if (list != null && !list.Contains(item))
+        ArgumentNullException.ThrowIfNull(list);
+
+        if (!list.Contains(item))
             list.Add(item);
     }
 
     /// <summary>
     /// Add multiple items to list
     /// </summary>
-    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <param name="collection">The collection to modify</param>
+    /// <param name="items">The items to add</param>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> or <paramref name="items"/> is <see langword="null"/></exception>
+    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T>? items)
     {
-        if (collection != null && items != null)
+        ArgumentNullException.ThrowIfNull(collection);
+
+        if (items != null)
         {
             foreach (var item in items)
                 collection.Add(item);
@@ -65,10 +87,15 @@ public static class CollectionExtensions
     /// <summary>
     /// Remove items matching predicate
     /// </summary>
+    /// <typeparam name="T">The type of elements in the list</typeparam>
+    /// <param name="list">The list to modify</param>
+    /// <param name="predicate">The predicate to match items for removal</param>
+    /// <returns>The number of items removed</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="list"/> or <paramref name="predicate"/> is <see langword="null"/></exception>
     public static int RemoveWhere<T>(this IList<T> list, Func<T, bool> predicate)
     {
-        if (list == null)
-            return 0;
+        ArgumentNullException.ThrowIfNull(list);
+        ArgumentNullException.ThrowIfNull(predicate);
 
         var itemsToRemove = list.Where(predicate).ToList();
         foreach (var item in itemsToRemove)
@@ -80,10 +107,18 @@ public static class CollectionExtensions
     /// <summary>
     /// Get distinct items based on key selector
     /// </summary>
-    public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> collection, Func<T, TKey> keySelector)
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <typeparam name="TKey">The type of key to distinguish by</typeparam>
+    /// <param name="collection">The source collection</param>
+    /// <param name="keySelector">Function to extract key from each element</param>
+    /// <returns>Collection containing only distinct elements based on key</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> or <paramref name="keySelector"/> is <see langword="null"/></exception>
+    public static IEnumerable<T> DistinctBy<T, TKey>(
+        this IEnumerable<T> collection,
+        Func<T, TKey> keySelector)
     {
-        if (collection == null)
-            return Enumerable.Empty<T>();
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(keySelector);
 
         var seen = new HashSet<TKey>();
         return collection.Where(item => seen.Add(keySelector(item)));
@@ -92,10 +127,20 @@ public static class CollectionExtensions
     /// <summary>
     /// Chunk collection into smaller collections of specified size
     /// </summary>
-    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> collection, int chunkSize)
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <param name="collection">The source collection to chunk</param>
+    /// <param name="chunkSize">The maximum size of each chunk</param>
+    /// <returns>Collection of chunks, each containing up to <paramref name="chunkSize"/> items</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="chunkSize"/> is less than 1</exception>
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(
+        this IEnumerable<T> collection,
+        int chunkSize)
     {
-        if (collection == null || chunkSize <= 0)
-            yield break;
+        ArgumentNullException.ThrowIfNull(collection);
+
+        if (chunkSize <= 0)
+            throw new ArgumentOutOfRangeException(nameof(chunkSize), "Chunk size must be positive");
 
         var chunk = new List<T>(chunkSize);
         foreach (var item in collection)
@@ -115,10 +160,18 @@ public static class CollectionExtensions
     /// <summary>
     /// Safely get value from dictionary
     /// </summary>
-    public static TValue? SafeGet<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) where TValue : class
+    /// <typeparam name="TKey">The type of keys in the dictionary</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary</typeparam>
+    /// <param name="dictionary">The dictionary to search</param>
+    /// <param name="key">The key to look up</param>
+    /// <returns>The value associated with the key, or <see langword="null"/> if not found or dictionary is null</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> or <paramref name="key"/> is <see langword="null"/></exception>
+    public static TValue? SafeGet<TKey, TValue>(
+        this IDictionary<TKey, TValue> dictionary,
+        TKey key) where TValue : class
     {
-        if (dictionary == null || key == null)
-            return null;
+        ArgumentNullException.ThrowIfNull(dictionary);
+        ArgumentNullException.ThrowIfNull(key);
 
         return dictionary.TryGetValue(key, out var value) ? value : null;
     }
@@ -126,10 +179,20 @@ public static class CollectionExtensions
     /// <summary>
     /// Get value from dictionary or return default
     /// </summary>
-    public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+    /// <typeparam name="TKey">The type of keys in the dictionary</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary</typeparam>
+    /// <param name="dictionary">The dictionary to search</param>
+    /// <param name="key">The key to look up</param>
+    /// <param name="defaultValue">The default value to return if key is not found</param>
+    /// <returns>The value associated with the key, or <paramref name="defaultValue"/> if not found</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> or <paramref name="key"/> is <see langword="null"/></exception>
+    public static TValue GetValueOrDefault<TKey, TValue>(
+        this IDictionary<TKey, TValue> dictionary,
+        TKey key,
+        TValue defaultValue)
     {
-        if (dictionary == null || key == null)
-            return defaultValue;
+        ArgumentNullException.ThrowIfNull(dictionary);
+        ArgumentNullException.ThrowIfNull(key);
 
         return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
     }
@@ -138,15 +201,24 @@ public static class CollectionExtensions
     /// Convert collection to dictionary with duplicate key handling
     /// When duplicates occur, the last value wins
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <typeparam name="TKey">The type of keys in the resulting dictionary</typeparam>
+    /// <typeparam name="TValue">The type of values in the resulting dictionary</typeparam>
+    /// <param name="collection">The source collection</param>
+    /// <param name="keySelector">Function to extract key from each element</param>
+    /// <param name="valueSelector">Function to extract value from each element</param>
+    /// <returns>A dictionary with keys mapped to values from the collection</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/>, <paramref name="keySelector"/>, or <paramref name="valueSelector"/> is <see langword="null"/></exception>
     public static Dictionary<TKey, TValue> ToSafeDictionary<T, TKey, TValue>(
         this IEnumerable<T> collection,
         Func<T, TKey> keySelector,
         Func<T, TValue> valueSelector) where TKey : notnull
     {
-        var result = new Dictionary<TKey, TValue>();
-        if (collection == null)
-            return result;
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(keySelector);
+        ArgumentNullException.ThrowIfNull(valueSelector);
 
+        var result = new Dictionary<TKey, TValue>();
         foreach (var item in collection)
         {
             var key = keySelector(item);
@@ -160,10 +232,18 @@ public static class CollectionExtensions
     /// <summary>
     /// Get the item with maximum value
     /// </summary>
-    public static T? MaxBy<T, TValue>(this IEnumerable<T> collection, Func<T, TValue> selector) where TValue : IComparable<TValue>
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <typeparam name="TValue">The type of comparable value</typeparam>
+    /// <param name="collection">The source collection</param>
+    /// <param name="selector">Function to extract comparable value from each element</param>
+    /// <returns>The item with the maximum value, or <see langword="null"/> if collection is empty</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> or <paramref name="selector"/> is <see langword="null"/></exception>
+    public static T? MaxBy<T, TValue>(
+        this IEnumerable<T> collection,
+        Func<T, TValue> selector) where TValue : IComparable<TValue>
     {
-        if (collection.IsNullOrEmpty())
-            return default;
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(selector);
 
         T? maxItem = default;
         var maxValue = default(TValue);
@@ -184,10 +264,18 @@ public static class CollectionExtensions
     /// <summary>
     /// Get the item with minimum value
     /// </summary>
-    public static T? MinBy<T, TValue>(this IEnumerable<T> collection, Func<T, TValue> selector) where TValue : IComparable<TValue>
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <typeparam name="TValue">The type of comparable value</typeparam>
+    /// <param name="collection">The source collection</param>
+    /// <param name="selector">Function to extract comparable value from each element</param>
+    /// <returns>The item with the minimum value, or <see langword="null"/> if collection is empty</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> or <paramref name="selector"/> is <see langword="null"/></exception>
+    public static T? MinBy<T, TValue>(
+        this IEnumerable<T> collection,
+        Func<T, TValue> selector) where TValue : IComparable<TValue>
     {
-        if (collection.IsNullOrEmpty())
-            return default;
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(selector);
 
         T? minItem = default;
         var minValue = default(TValue);
@@ -209,12 +297,17 @@ public static class CollectionExtensions
     /// Partition collection into two based on predicate
     /// Returns matching and non-matching items
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <param name="collection">The source collection</param>
+    /// <param name="predicate">The predicate to partition by</param>
+    /// <returns>A tuple containing matching and non-matching items</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> or <paramref name="predicate"/> is <see langword="null"/></exception>
     public static (IEnumerable<T> Matching, IEnumerable<T> NotMatching) Partition<T>(
         this IEnumerable<T> collection,
         Func<T, bool> predicate)
     {
-        if (collection == null)
-            return (Enumerable.Empty<T>(), Enumerable.Empty<T>());
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(predicate);
 
         var matching = new List<T>();
         var notMatching = new List<T>();
@@ -233,10 +326,17 @@ public static class CollectionExtensions
     /// <summary>
     /// Get intersection of two collections
     /// </summary>
-    public static IEnumerable<T> GetIntersection<T>(this IEnumerable<T> collection1, IEnumerable<T> collection2)
+    /// <typeparam name="T">The type of elements in the collections</typeparam>
+    /// <param name="collection1">The first collection</param>
+    /// <param name="collection2">The second collection</param>
+    /// <returns>Collection containing items present in both collections</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection1"/> or <paramref name="collection2"/> is <see langword="null"/></exception>
+    public static IEnumerable<T> GetIntersection<T>(
+        this IEnumerable<T> collection1,
+        IEnumerable<T> collection2)
     {
-        if (collection1.IsNullOrEmpty() || collection2.IsNullOrEmpty())
-            return Enumerable.Empty<T>();
+        ArgumentNullException.ThrowIfNull(collection1);
+        ArgumentNullException.ThrowIfNull(collection2);
 
         var set2 = new HashSet<T>(collection2);
         return collection1.Where(set2.Contains).Distinct();
@@ -245,13 +345,17 @@ public static class CollectionExtensions
     /// <summary>
     /// Get difference of two collections (items in first but not in second)
     /// </summary>
-    public static IEnumerable<T> GetDifference<T>(this IEnumerable<T> collection1, IEnumerable<T> collection2)
+    /// <typeparam name="T">The type of elements in the collections</typeparam>
+    /// <param name="collection1">The first collection</param>
+    /// <param name="collection2">The second collection</param>
+    /// <returns>Collection containing items present in first but not in second</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection1"/> or <paramref name="collection2"/> is <see langword="null"/></exception>
+    public static IEnumerable<T> GetDifference<T>(
+        this IEnumerable<T> collection1,
+        IEnumerable<T> collection2)
     {
-        if (collection1.IsNullOrEmpty())
-            return Enumerable.Empty<T>();
-
-        if (collection2.IsNullOrEmpty())
-            return collection1;
+        ArgumentNullException.ThrowIfNull(collection1);
+        ArgumentNullException.ThrowIfNull(collection2);
 
         var set2 = new HashSet<T>(collection2);
         return collection1.Where(item => !set2.Contains(item)).Distinct();
@@ -260,10 +364,13 @@ public static class CollectionExtensions
     /// <summary>
     /// Flatten nested collections
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collections</typeparam>
+    /// <param name="collection">The collection of collections to flatten</param>
+    /// <returns>Flattened collection containing all elements</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/></exception>
     public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> collection)
     {
-        if (collection == null)
-            return Enumerable.Empty<T>();
+        ArgumentNullException.ThrowIfNull(collection);
 
         return collection.SelectMany(x => x ?? Enumerable.Empty<T>());
     }
@@ -271,10 +378,25 @@ public static class CollectionExtensions
     /// <summary>
     /// Create a batched/paged version of collection for pagination
     /// </summary>
-    public static IEnumerable<T> Page<T>(this IEnumerable<T> collection, int pageNumber, int pageSize)
+    /// <typeparam name="T">The type of elements in the collection</typeparam>
+    /// <param name="collection">The source collection</param>
+    /// <param name="pageNumber">The one-based page number to retrieve</param>
+    /// <param name="pageSize">The number of items per page</param>
+    /// <returns>Collection containing items for the specified page</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="pageNumber"/> or <paramref name="pageSize"/> is less than 1</exception>
+    public static IEnumerable<T> Page<T>(
+        this IEnumerable<T> collection,
+        int pageNumber,
+        int pageSize)
     {
-        if (collection.IsNullOrEmpty() || pageNumber < 1 || pageSize < 1)
-            return Enumerable.Empty<T>();
+        ArgumentNullException.ThrowIfNull(collection);
+
+        if (pageNumber < 1)
+            throw new ArgumentOutOfRangeException(nameof(pageNumber), "Page number must be positive");
+
+        if (pageSize < 1)
+            throw new ArgumentOutOfRangeException(nameof(pageSize), "Page size must be positive");
 
         var skip = (pageNumber - 1) * pageSize;
         return collection.Skip(skip).Take(pageSize);
