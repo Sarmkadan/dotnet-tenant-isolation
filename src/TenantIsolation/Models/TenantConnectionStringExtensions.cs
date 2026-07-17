a87e7a1e3e0bcb541b1de0bc470859f2c848eda5
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 14:49:41 +0300

    Small refactor - TenantConnectionStringExtensions

--- src/TenantIsolation/Models/TenantConnectionStringExtensions.cs
+++ src/TenantIsolation/Models/TenantConnectionStringExtensions.cs
@@ -17,20 +17,24 @@ namespace TenantIsolation.Models
         {
             ArgumentNullException.ThrowIfNull(connectionString);
 
-            if (string.IsNullOrEmpty(connectionString.ServerHost))
-                throw new ArgumentException("ServerHost must not be null or empty.", nameof(connectionString.ServerHost));
+            _ = connectionString.ServerHost ?? throw new ArgumentException(
+                "ServerHost must not be null or empty.", nameof(connectionString));
 
-            if (connectionString.ServerPort is null or <= 0 or > 65535)
-                throw new ArgumentException("ServerPort must be between 1 and 65535.", nameof(connectionString.ServerPort));
+            _ = connectionString.ServerPort is null or <= 0 or > 65535
+                ? throw new ArgumentException("ServerPort must be between 1 and 65535.", nameof(connectionString))
+                : true;
 
-            if (string.IsNullOrEmpty(connectionString.DatabaseType))
-                throw new ArgumentException("DatabaseType must not be null or empty.", nameof(connectionString.DatabaseType));
+            _ = connectionString.DatabaseType ?? throw new ArgumentException(
+                "DatabaseType must not be null or empty.", nameof(connectionString));
 
-            if (connectionString.DatabaseType is not ("SqlServer" or "PostgreSQL" or "MySQL"))
-                throw new ArgumentException("DatabaseType must be a supported type: SqlServer, PostgreSQL, MySQL.", nameof(connectionString.DatabaseType));
+            _ = connectionString.DatabaseType is "SqlServer" or "PostgreSQL" or "MySQL"
+                ? true
+                : throw new ArgumentException(
+                    "DatabaseType must be a supported type: SqlServer, PostgreSQL, MySQL.", nameof(connectionString));
 
-            if (!string.IsNullOrEmpty(connectionString.SchemaName) && connectionString.SchemaName.Length > 128)
-                throw new ArgumentException("SchemaName must not exceed 128 characters.", nameof(connectionString.SchemaName));
+            _ = !string.IsNullOrEmpty(connectionString.SchemaName) && connectionString.SchemaName.Length > 128
+                ? throw new ArgumentException("SchemaName must not exceed 128 characters.", nameof(connectionString))
+                : true;
         }
 
         /// <summary>
@@ -38,6 +42,7 @@ namespace TenantIsolation.Models
         /// </summary>
         /// <param name="connectionString">The connection string to generate a display name for.</param>
         /// <returns>A formatted string representing the connection string.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="connectionString"/> is null.</exception>
         public static string ToDisplayName(this TenantConnectionString connectionString)
         {
             ArgumentNullException.ThrowIfNull(connectionString);
