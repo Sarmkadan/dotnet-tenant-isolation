1c0d2fff3a2f8a448354ea8100b824a9c8def294
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 13:25:03 +0300

    cleanup in WebhookPayloadJsonExtensions

--- src/TenantIsolation/Integration/WebhookPayloadJsonExtensions.cs
+++ src/TenantIsolation/Integration/WebhookPayloadJsonExtensions.cs
@@ -28,15 +28,27 @@ public static class WebhookPayloadJsonExtensions
     /// <param name="value">The webhook payload to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the webhook payload.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this WebhookPayload value, bool indented = false)
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this WebhookPayload value, bool indented = false) =>
+        ToJson(value, out var _);
+
+    /// <summary>
+    /// Serializes the <see cref="WebhookPayload"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The webhook payload to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the webhook payload.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this WebhookPayload value, out JsonSerializerOptions _)
     {
         ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
-            : _jsonSerializerOptions;
+        var options = new JsonSerializerOptions(_jsonSerializerOptions)
+        {
+            WriteIndented = false
+        };
 
+        _ = options;
         return JsonSerializer.Serialize(value, options);
     }
 
@@ -44,8 +56,8 @@ public static class WebhookPayloadJsonExtensions
     /// Deserializes a JSON string to a <see cref="WebhookPayload"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A deserialized <see cref="WebhookPayload"/> instance, or null if the JSON is invalid.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <returns>A deserialized <see cref="WebhookPayload"/> instance, or <see langword="null"/> if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static WebhookPayload? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -65,8 +77,8 @@ public static class WebhookPayloadJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized payload if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static bool TryFromJson(string json, out WebhookPayload? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
