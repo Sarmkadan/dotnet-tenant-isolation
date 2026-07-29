54961ca6cfbe07f83ea249e1300c0b9e8ca4a924
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 17:04:13 +0300

    chore(dotnet-tenant-isolation.tests): tweak CollectionExtensionsTests
    also touched a couple of call sites.

---
+++ tests/dotnet-tenant-isolation.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using TenantIsolation.Utilities;
+
+namespace dotnet_tenant_isolation.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        // Test methods go here
+    }
+}
