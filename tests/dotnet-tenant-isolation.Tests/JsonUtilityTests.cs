0d89430f43a4a0e46e894d5606a652ad287b9dc1
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 13:35:16 +0300

    chore: JsonUtilityTests.cs

---
+++ tests/dotnet-tenant-isolation.Tests/JsonUtilityTests.cs
@@ -0,0 +1,102 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using TenantIsolation.Utilities;
+
+namespace dotnet_tenant_isolation.Tests
+{
+    public class JsonUtilityTests
+    {
+        [Fact]
+        public void Serialize_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void SerializePretty_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Deserialize_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void DeserializeSafe_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void DeserializeToDynamic_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ConvertToDictionary_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void MergeJsonObjects_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ExtractValue_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void IsValidJson_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void PrettyPrint_Happy_Path_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Serialize_Null_Input_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Deserialize_Null_Input_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void DeserializeSafe_Null_Input_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void DeserializeToDynamic_Null_Input_Test()
+        {
+            // Test implementation
+        }
+    }
+}
