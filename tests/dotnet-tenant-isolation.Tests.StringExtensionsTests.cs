72b83280c27d6af96fe8d6e1411b41d4e7f8c55c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 19:50:00 +0300

    Tweak dotnet-tenant-isolation.Tests.StringExtensionsTests

---
+++ tests/dotnet-tenant-isolation.Tests.StringExtensionsTests.cs
@@ -0,0 +1,135 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dotnet_tenant_isolation.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ToSlug_HappyPath_ReturnsSlug()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ToSlug_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ToSlug_WhitespaceInput_ReturnsEmptyString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Truncate_HappyPath_ReturnsTruncatedString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Truncate_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void IsValidEmail_HappyPath_ReturnsTrue()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void IsValidEmail_NullInput_ReturnsFalse()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void IsValidUrl_HappyPath_ReturnsTrue()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void IsValidUrl_NullInput_ReturnsFalse()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void SafeSubstring_HappyPath_ReturnsSubstring()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void SafeSubstring_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void RemoveSpecialCharacters_HappyPath_ReturnsAlphanumericString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void RemoveSpecialCharacters_NullInput_ReturnsEmptyString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void MaskSensitiveData_HappyPath_ReturnsMaskedString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void MaskSensitiveData_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ToPascalCase_HappyPath_ReturnsPascalCaseString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ToPascalCase_NullInput_ReturnsEmptyString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ToHumanReadable_HappyPath_ReturnsHumanReadableString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ToHumanReadable_NullInput_ReturnsOriginalString()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void GetDeterministicHashCode_HappyPath_ReturnsHashCode()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void GetDeterministicHashCode_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+    }
+}
